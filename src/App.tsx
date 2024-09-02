8c2ce199677f552c95c69f7d20d1e617f78f63c8
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2024-09-02 11:37:25 +0300

    feat: select color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import './App.css';
 
-const tg = window.Telegram?.WebApp;
+// const tg = window.Telegram?.WebApp;
 
 function App() {
 
-  useEffect(() => {
-      tg.ready(); // Инициализация
-  }, []);
+  // useEffect(() => {
+  //     tg.ready(); // Инициализация
+  // }, []);
 
 
-  const onClose = () => {
-    tg.close()
-  }
+  // const onClose = () => {
+  //   tg.close()
+  // }
 
   return (
-    <div className="bg-slate-700">
-      <div className="">Добров пожаловать!<br /> Выберите формат ввода времени</div>
+    <div className="bg-[#26425A] w-screen h-screen">
+      <div className="text-center pt-8">Добро пожаловать!</div>
+      <div className="text-center">Выберите формат ввода времени</div>
       <div className="">      
-        <button className='bg-red-600 border-2 border-black rounded-2xl p-1' onClick={onClose}>закрыть</button>
       </div>
-      <button className='bg-emerald-600 align-middle '>Учёт времени по месяцам</button>
-      <button className='bg-emerald-600'>Учёт времени по проектам</button>
+      <div className="flex justify-evenly pt-5">
+      <button className='bg-emerald-600 rounded-xl p-2'>Учёт времени по месяцам</button>
+
+      <button className='bg-emerald-600 rounded-xl p-2'>Учёт времени по проектам</button>
+      </div>
     </div>
   );
 }
