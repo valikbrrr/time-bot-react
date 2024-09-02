d94a3d223664568ff4fea75f3f8fba16a6975562
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2024-09-02 11:15:09 +0300

    test connect with tg (3)

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,9 @@ function App() {
   return (
     <div className="bg-slate-700">
       <div className="">Добров пожаловать!<br /> Выберите формат ввода времени</div>
-      <div className="">      <button className='bg-red-600 border-2 border-black rounded-2xl p-1' onClick={onClose}>закрыть
-      </button></div>
+      <div className="">      
+        <button className='bg-red-600 border-2 border-black rounded-2xl p-1' onClick={onClose}>закрыть</button>
+      </div>
       <button className='bg-emerald-600 align-middle '>Учёт времени по месяцам</button>
       <button className='bg-emerald-600'>Учёт времени по проектам</button>
     </div>
