c6434eb6889a5c70d904242758953ebc527f707a
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2024-09-02 10:58:16 +0300

    test connect tg

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,19 @@ function App() {
     }
   }, []);
 
+  console.log(tg);
+
   const onClose = () => {
-    if (tg) {
-      tg.close();
-    }
-  };
+    tg?.close()
+  }
 
   return (
     <div className="bg-slate-700">
-      <button className='bg-emerald-600' onClick={onClose}>закрыть</button>
-      <div className="bg-red-700">like</div>
+      <div className="">Добров пожаловать!<br /> Выберите формат ввода времени</div>
+      <div className="">      <button className='bg-red-600 border-2 border-black rounded-2xl p-1' onClick={onClose}>закрыть
+      </button></div>
+      <button className='bg-emerald-600 align-middle '>Учёт времени по месяцам</button>
+      <button className='bg-emerald-600'>Учёт времени по проектам</button>
     </div>
   );
 }
