e3638317984c2e67a09d1033032773036973a4ba
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2024-09-13 01:36:44 +0300

    feat: displaying month name

--- src/pages/Month/AddHoursMonth.tsx
+++ src/pages/Month/AddHoursMonth.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import BackArrow from '../../assets/BackArrow';
 
 const AddHoursMonth = () => {
-    console.log("work AddHoursMonth");
     const [months, setMonths] = useState([]);
+    const [selectedMonth, setSelectedMonth] = useState <number | null>(null); // Состояние для выбранного месяца
 
     useEffect(() => {
         const fetchMonths = async () => {
@@ -20,6 +20,11 @@ const AddHoursMonth = () => {
         fetchMonths();
     }, []);
 
+    // Функция для обработки нажатия на кнопку
+    const handleButtonClick = (index: number) => {
+        setSelectedMonth(index);
+    };
+
     return (
         <div className="bg-[#26425A] w-full h-full min-h-screen min-w-screen overflow-hidden flex flex-col justify-between">
             <BackArrow lastPage={"/mouthbranch"} />
@@ -29,13 +34,27 @@ const AddHoursMonth = () => {
             <div className="flex justify-center mb-40">
                 <div className="w-[70%]">
                     <div className="flex flex-col items-center">
-                    {months.map((month, index) => (
-                        <button
-                        className='bg-blue-500 text-white rounded-xl p-3 w-full mb-4 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-600 shadow-lg hover:shadow-xl' key={index}
-                        >
-                        {month}
-                        </button>
-                    ))}
+                        
+                        {months.map((month, index) => {
+                            let selectedButton;
+                            if (selectedMonth === index) {
+                                selectedButton = index === 0 ? month : index === 1 ? month : `${month}`;
+                                console.log(selectedButton);
+                            }
+                            // if (selectedMonth === index) {
+                            //     selectedButton = 1
+                            // }
+
+                            return (
+                                <button
+                                    className={`bg-blue-500 text-white rounded-xl p-3 w-full mb-4 transition duration-300 ease-in-out transform hover:scale-105 shadow-lg hover:shadow-xl`}
+                                    key={index}
+                                    onClick={() => handleButtonClick(index)} // Обработка нажатия
+                                >
+                                    {month}
+                                </button>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
@@ -44,4 +63,3 @@ const AddHoursMonth = () => {
 };
 
 export default AddHoursMonth;
-
