06d473f0955c1154f3331e0cf999439efeab2ba6
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2024-09-18 00:03:20 +0300

    feat: try 4)

--- src/pages/Month/ViewHoursMonth.tsx
+++ src/pages/Month/ViewHoursMonth.tsx
@@ -68,7 +68,7 @@ const ViewHoursMonth = () => {
         dispatch(selectMonthView(month));
     };
 
-    // const a = tg.initDataUnsafe.user
+    const a = tg.initDataUnsafe.user?.toString
 
     return (
         <div className="">
@@ -79,6 +79,7 @@ const ViewHoursMonth = () => {
                         <div className="text-center text-white text-3xl mb-4">
                             Ваши часы за {selectedMonthView} - "{hours}" 
                             и ид - "{id}""
+                            ААА - {a}
                         </div>
                     </div>
                 </div>
