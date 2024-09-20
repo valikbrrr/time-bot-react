07758326e01b2ff27e39bd1d72e64be53c7f306a
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2024-09-20 18:44:26 +0300

    feat: create backToHomepage

--- src/pages/Month/AddHoursMonth.tsx
+++ src/pages/Month/AddHoursMonth.tsx
@@ -4,22 +4,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectMonth } from '../../store/monthSlice';
 import BackArrow from '../../assets/BackArrow';
 import React from 'react';
+import { selectMonthView } from '../../store/monthViewSlice';
+import { useNavigate } from 'react-router-dom';
 
 
 const AddHoursMonth: React.FC = () => {
+
+    const navigate = useNavigate()
     const dispatch = useDispatch();
     const [months, setMonths] = useState<string[]>([]);
     const [hours, setHours] = useState<string>('');
     const selectedMonth = useSelector((state: any) => state.month.selectedMonth); 
+    const [backToHomepage, setBackToHomepage] = useState<boolean>(false); 
     
     useEffect(() => {
         const fetchMonths = async () => {
             try {
                 const url = process.env.REACT_APP_API_URL;
-                console.log(`url - ${url}`);
-                
                 const response = await fetch(`${url}/api/current-month`);
-                const data = await response.json();
+                const data = await response.json(); // json?
                 setMonths(data);
             } catch (error) {
                 console.error('Ошибка при получении данных:', error);
@@ -29,22 +32,29 @@ const AddHoursMonth: React.FC = () => {
         fetchMonths();
     }, []);
 
-    const handleMonthSelect = (month: string) => {
-        dispatch(selectMonth(month));
-    };
 
+    useEffect(() => {
+        setHours("")
+        dispatch(selectMonthView(''))
+    }, [dispatch])
+
+    
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         const regex = /^(?:[1-9][0-9]{0,2}|[1-7][0-4][0-4]?)$/;
-
+        
         if (regex.test(value) || value === '') {
             setHours(value);
         }
     };
-
+    
     const handleSubmit = async () => {
-        if (!selectedMonth || !hours) return;
-
+        setBackToHomepage(true)
+        console.log(`selectMonth - ${selectedMonth}`);
+        console.log(`hours - ${hours}`);
+        
+        if (!selectedMonth) return;
+        console.log("workkk");
         try {
             const response = await fetch('http://localhost:3001/api/submit-hours', {
                 method: 'POST',
@@ -67,13 +77,39 @@ const AddHoursMonth: React.FC = () => {
             console.error('Ошибка при отправке данных:', error);
         }
     };
+    
+    const handleMonthSelect = (month: string) => {
+        dispatch(selectMonth(month));
+    };
+
+    if (backToHomepage) {
+        return (
+            <div className="bg-[#26425A] w-full h-full min-h-screen min-w-screen overflow-hidden flex flex-col justify-center">
+                <div className="pt-32 px-[10%]">
+                    <div className="text-center text-white text-3xl mb-8">
+                        Ваши часы записаны!
+                    </div>
+                </div>
+                <div className="flex justify-center mb-40">
+                    <div className="w-[70%]">
+                        <div className="flex flex-col items-center">
+                            <button className="bg-blue-500 text-white rounded-xl p-3 w-full mb-4 transition duration-300 ease-in-out transform hover:scale-105 shadow-lg hover:shadow-xl"
+                                onClick={() => navigate("/")}>
+                                Вернуться на главную страницу
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="">
             {selectedMonth ? (
                 <div className="bg-[#26425A] w-full h-full min-h-screen min-w-screen overflow-hidden flex flex-col">
                     <BackArrow lastPage={"/mouthbranch"} />
-                    <div className="pt-8 px-[10%]">
+                    <div className="pt-20 px-[10%]">
                         <div className="text-center text-white text-3xl mb-4">Введите количество часов</div>
                     </div>
                     <div className="text-white text-xl flex justify-center">
